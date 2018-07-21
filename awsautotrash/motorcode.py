c9cc57dda7146cfbdc891e8cc74ca5ab9ecba887
Eshita <e.shrawan@gmail.com>
2018-07-21 15:11:26 +0530

    added code for relay

--- awsautotrash/motorcode.py
+++ awsautotrash/motorcode.py
@@ -1,8 +1,27 @@
-import l293d.driver as l293d
+#enables change in two direcitons
+#import l293d.driver as l293d
 # Motor 1 uses Pin 22, Pin 18, Pin 16
-motor1 = l293d.motor(22,18,16)
+#motor1 = l293d.motor(22,18,16)
 # Run the motors so visible
-for i in range(0,150):
-  motor1.clockwise()
-l293d.cleanup()
+#for i in range(0,150):
+  #motor1.clockwise()
+#293d.cleanup()
 
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+# Set relay pins as output
+GPIO.setup(18, GPIO.OUT)
+
+while (True):
+
+    # Turn relay ON
+    GPIO.output(18, GPIO.HIGH)
+    # Sleep for 5 seconds
+    sleep(5)
+    # Turn all relay OFF
+    GPIO.output(18, GPIO.LOW)
+    # Sleep for 5 seconds
+    sleep(5)
